890758b845b8b2712c2d5a8e706028154e70ae66
proghax333 <skillzworldtech@gmail.com>
2023-05-26 11:12:53 +0530

    feat: add payload selection feature

--- apps/frontend/src/pages/dashboard/payloads.jsx
+++ apps/frontend/src/pages/dashboard/payloads.jsx
@@ -19,7 +19,7 @@ import { PayloadDelete } from "~/modules/payloads/components/delete-payload";
 import { useMemoSortedCollection } from "~/modules/payloads/hooks/payloads.hooks";
 import { NodeItem } from "~/modules/payloads/components/node-item";
 
-function TreeCollection({ node: node_, level = 0 }) {
+function TreeCollection({ node: node_, level = 0, selected, onPayloadOpen, onPayloadClose }) {
   const LEFT_ICON_SIZE = 18;
   const [isOpen, setIsOpen] = React.useState(false);
 
@@ -39,6 +39,14 @@ function TreeCollection({ node: node_, level = 0 }) {
       : ChevronRight
     : InsertDriveFile;
 
+  function onSelect() {
+    if(selected?.id === node.id) {
+      onPayloadClose(node);
+    } else {
+      onPayloadOpen(node);
+    }
+  }
+  
   return <Box>
     <NodeItem
       pl={`${4 + level * 12 + (nodeChildren == undefined ? 0 : 0)}px`}
@@ -46,11 +54,12 @@ function TreeCollection({ node: node_, level = 0 }) {
       cursor={"pointer"}
       onClick={handleToggle}
       _hover={{
-        background: "gray.800"
+        background: selected?.id === node.id ? "gray.700" : "gray.800"
       }}
+      background={selected?.id === node.id ? "gray.700" : null}
     >
       <IconComponent m={1} size={LEFT_ICON_SIZE} style={{minWidth: LEFT_ICON_SIZE}} />
-      <Text my={1} px={1} flex={1}>{node.name}</Text>
+      <Text my={1} px={1} flex={1} onClick={onSelect}>{node.name}</Text>
       <PayloadEdit payload={node} />
       <PayloadDelete payload={node} />
     </NodeItem>
@@ -63,6 +72,10 @@ function TreeCollection({ node: node_, level = 0 }) {
             key={`tree-node-${child.id}`}
             level={level + 1}
             node={child}
+
+            selected={selected}
+            onPayloadOpen={onPayloadOpen}
+            onPayloadClose={onPayloadClose}
           />
         })}
       </Box>
@@ -86,6 +99,16 @@ export default function MainContent() {
   const getPayloadsQuery = useGetPayloadsQuery();
   const payloads = useMemoSortedCollection(getPayloadsQuery.data?.itemByDomain("payload")?.data);
   const createPayloadModal = useDisclosure();
+
+  const [selected, setSelected] = React.useState(null);
+
+  function onPayloadOpen(payload) {
+    console.log(payload);
+    setSelected(payload);
+  }
+  function onPayloadClose(payload) {
+    setSelected(null);
+  }
 
   return <Flex
     w="full"
@@ -129,6 +152,10 @@ export default function MainContent() {
               key={`tree-node-${node.id}`}
               node={node}
               level={0}
+
+              selected={selected}
+              onPayloadOpen={onPayloadOpen}
+              onPayloadClose={onPayloadClose}
             />;
           })}
         </Box>
