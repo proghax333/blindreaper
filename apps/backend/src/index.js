577db7dc2c02371379e343332f3c85fdf9f7cbc4
proghax333 <skillzworldtech@gmail.com>
2023-06-06 13:12:15 +0530

    fix: add dynamic port to express app

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -267,7 +267,7 @@ async function main() {
   });
 
 
-  const PORT = 5001;
+  const PORT = process.env.PORT || 5001;
   app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}.`);
   });
