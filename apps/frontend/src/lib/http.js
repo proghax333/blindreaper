2bc0566e26dacc0084bdbb96097d4ced6e3d7f85
proghax333 <skillzworldtech@gmail.com>
2023-06-06 12:27:04 +0530

    feat: add environment variable support

--- apps/frontend/src/lib/http.js
+++ apps/frontend/src/lib/http.js
@@ -4,7 +4,7 @@ import { reject, transformK } from "~/lib/utils";
 
 import * as R from "ramda";
 
-export const API_BASE_URL = "http://localhost:5001";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
